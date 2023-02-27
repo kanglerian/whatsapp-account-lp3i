93a4d411a5f4928f1817e3a1e0725327d6ec8c9f
kampusinformatika <kanglerian.business@gmail.com>
2023-02-27 13:41:37 +0700

    first

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 5000;
+
+const data = require('./db.json');
+
+
+app.use(cors());
+
+app.get('/', async (req, res) => {
+    res.send("Whatsapp Accounts");
+});
+
+app.get('/api/accounts/:id', async (req, res) => {
+    let result = [];
+    for (let i = 0; i < data.accounts.length; i++) {
+        if (data.accounts[i].phone == req.params.id) {
+            let dataUser = {
+                phone: data.accounts[i].phone,
+                status: data.accounts[i].status,
+            }
+            result.push(dataUser);
+        }
+    }
+    return res.json(result);
+});
+
+
+app.listen(port, () => console.log(`Apps run on http://localhost:${port}`));
+
+module.express = app;
